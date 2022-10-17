68b8bd882d9e522b623a31045c2c16c241012bba
Lucaponz91 <lucapo1991@msn.com>
2022-10-17 16:25:45 +0200

    add a bit of css

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,10 +4,11 @@
     <div class="show_main">
         <div>
             <h1>{{$comic->title}}</h1>
+            <img src="{{$comic['thumb']}}" alt="img">
             <h4>{{$comic['price']}}</h4>
             <p>{{$comic['description']}} </p>
         </div>
-        <img src="{{asset('img/adv.jpg')}}" alt="">
+        <img class="ad" src="{{asset('img/adv.jpg')}}" alt="">
     </div>
 </div>
 
